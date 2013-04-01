85ef2b289e3dd74ee6e20617a6b6f0ac52c72283
Gloton <gloton@gmail.com>
2013-04-01 12:02:25 -0300

    Template - header ok Construccion del template. La seccion 1 esta ok.

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE HTML>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Home - Portage</title>
+	<link rel="styleSheet" href="css/reset.css" type="text/css" media="screen" />
+	<link rel="styleSheet" href="css/style.css" type="text/css" media="screen" />
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js" ></script>
+	<script src="js/fonts/cufon-yui.js" type="text/javascript"></script>
+	<script src="js/fonts/DIN_Alternate_Light_300.font.js" type="text/javascript"></script>
+	<script type="text/javascript">
+		Cufon.replace(".menu, .menu_topright",{fontFamily:"DIN Alternate Light"});
+	</script>
+	 <!--[if lt IE 9]>
+	<script type="text/javascript">
+	   document.createElement("nav");
+	   document.createElement("header");
+	   document.createElement("footer");
+	   document.createElement("section");
+	   document.createElement("article");
+	   document.createElement("aside");
+	   document.createElement("hgroup");
+	</script>
+	<![endif]-->
+</head>
+<body>
+<div id="seccion1">
+	<div class="contenedor">
+		<header>
+			<div id="col1">
+				<img src="images/logo-portage.png" alt="Logo Portage Tech" />
+			</div>
+			<div id="col2">
+				<ul class="menu">
+					<li><a href="#">NOSOTROS</a></li>
+					<li><a href="#">SOLUCIONES</a></li>
+					<li><a href="#">PROYECTOS</a></li>
+					<li><a href="#">NOTICIAS</a></li>
+				</ul>
+			</div>
+			<div id="col3">
+				<ul class="menu_topright">
+					<li><a href="#">ENGLISH</a></li>
+					<li class="separador_menu_top">|</li>
+					<li id="contact_top"><a href="#">CONTACTO</a></li>	
+				</ul>
+			</div>
+		</header>
+	</div>
+</div>
+<div id="seccion2">
+	<div class="contenedor"></div>
+</div>
+<div id="seccion3">
+	<div class="contenedor"></div>
+</div>
+<div id="seccion4">
+	<div class="contenedor"></div>
+</div>
+	
+</body>
+</html>
